2b50717585a3be8d074e48dc7962f1ad65c46198
curroramos <curro.ramosperez@gmail.com>
2025-05-04 17:22:28 -0500

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from './sections/About.jsx';
 import Footer from './sections/Footer.jsx';
 import Navbar from './sections/Navbar.jsx';
 import Contact from './sections/Contact.jsx';
-import Clients from './sections/Clients.jsx';
+// import Clients from './sections/Clients.jsx';
 import Projects from './sections/Projects.jsx';
 import WorkExperience from './sections/WorkExperience.jsx';
 import Hero from './sections/Hero.jsx';
